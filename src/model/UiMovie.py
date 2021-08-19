b6408afacfed690b8b10cbd66215e072f97acd98
jorgen saeter <jorgen.sater@gmail.com>
2021-08-19 18:36:36 +1000

    Added /getgenre api

---
+++ src/model/UiMovie.py
@@ -0,0 +1,5 @@
+class UiMovie:
+    def __init__(self, filename, uuid, genres):
+        self.filename = filename
+        self.uuid = uuid
+        self.genres = genres
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,1 +1,2 @@
 from routes import admin
+from routes import utility
